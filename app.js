66b76152314ef1456df13d5631385411b29ba08a
RITIK KAPOOR <61417822+rkapoor10@users.noreply.github.com>
2021-06-11 00:01:47 +0530

    ex11: replace the mock url

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var outputDiv = document.querySelector("#output")
 // outputDiv.innerText = "Ritik Kapoor"
 // console.log(outputDiv)
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationalURL(text)
 {
@@ -28,9 +28,11 @@ function clickHandler()
 
     //calling server for processing
     fetch(getTranslationalURL(inputText))
-    .then(response=>response.json())
-    .then(json=>console.log(json.contents.translated))
-    catch(errorHandler)
+    .then(response => response.json())
+    .then(json =>{
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;})
+    .catch(errorHandler)
   
 };
 
