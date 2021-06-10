d669c692d550577c3e59a5de90eb9ab9f1d3de34
RITIK KAPOOR <61417822+rkapoor10@users.noreply.github.com>
2021-06-10 23:04:15 +0530

    ex05: wire a div to show output

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var btnTranslate = document.querySelector("#btn-translate")
 var txtInput = document.querySelector("#txt-input")
-
-btnTranslate.addEventListener("click",function clickEventHandler()
-{
-    console.log("you clicked the button")
-    console.log("input given: ", txtInput.value);
+var outputDiv = document.querySelector("#output")
+// outputDiv.innerText = "Ritik Kapoor"
+console.log(outputDiv)
+btnTranslate.addEventListener("click", function clickEventHandler() {
+    // console.log("you clicked the button")
+    // console.log("input given: ", txtInput.value);
+    outputDiv.innerText = "banana translate of "+txtInput.value;
 })
