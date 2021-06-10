ba39263cc224c71178a3b81e9b3821dd8a50b795
RITIK KAPOOR <61417822+rkapoor10@users.noreply.github.com>
2021-06-10 23:39:44 +0530

    ex09: wire fetch call in app

--- app.js
+++ app.js
@@ -2,9 +2,30 @@ var btnTranslate = document.querySelector("#btn-translate")
 var txtInput = document.querySelector("#txt-input")
 var outputDiv = document.querySelector("#output")
 // outputDiv.innerText = "Ritik Kapoor"
-console.log(outputDiv)
-btnTranslate.addEventListener("click", function clickEventHandler() {
+// console.log(outputDiv)
+
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getTranslationalURL(text)
+{
+    return serverURL + "?" + "text=" + text;
+}
+
+
+function clickHandler()
+{
     // console.log("you clicked the button")
     // console.log("input given: ", txtInput.value);
-    outputDiv.innerText = "banana translate of "+txtInput.value;
-})
+    // outputDiv.innerText = "banana translate of "+txtInput.value;
+
+    //taking input
+    var inputText = txtInput.value
+
+    //calling server for processing
+    fetch(getTranslationalURL(inputText))
+    .then(response=>response.json())
+    .then(json=>console.log(json.contents.translated))
+  
+};
+
+btnTranslate.addEventListener("click", clickHandler)
